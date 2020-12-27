da16fcba9b1f3d386c6b452cd664978c142a2850
Gerald Low <bcl15@ic.ac.uk>
2020-12-27 18:04:56 +0000

    Initial commit for Beskar

---
+++ app/src/main/java/com/example/beskar/ui/home/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.beskar.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.beskar.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
+}
