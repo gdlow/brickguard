cf65b7888f3bbb5bbfcab4234ff94158d6f617fe
Gerald Low <bcl15@ic.ac.uk>
2020-12-28 13:21:31 +0000

    Added expand more button

--- app/src/main/java/com/example/beskar/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/beskar/ui/home/HomeFragment.java
@@ -8,10 +8,8 @@ import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.danimahardhika.android.helpers.animation.AnimationHelper;
 import com.example.beskar.R;
 
 import java.util.ArrayList;
@@ -21,9 +19,6 @@ import java.util.List;
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
-    private List<View> layoutList = new ArrayList<>();
-    private int currentStep = 0;
-    private int completeStep = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,24 +31,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     //initialize component
     private void initComponent(View view) {
-        // populate layout field
-        layoutList.add(view.findViewById(R.id.activity_steppers_layout_step1));
-        layoutList.add(view.findViewById(R.id.activity_steppers_layout_step2));
-        layoutList.add(view.findViewById(R.id.activity_steppers_layout_step3));
-
-        //set visibility of view
-        for (View v : layoutList) {
-            v.setVisibility(View.GONE);
-        }
-
-        //display visible step
-        layoutList.get(0).setVisibility(View.VISIBLE);
-
-        //register buttons
         List<View> buttons = new ArrayList<>(Arrays.asList(
-                view.findViewById(R.id.activity_steppers_txt_label_step1),
-                view.findViewById(R.id.activity_steppers_txt_label_step2),
-                view.findViewById(R.id.activity_steppers_txt_label_step3)
+                view.findViewById(R.id.activity_steppers_container_step1),
+                view.findViewById(R.id.activity_steppers_container_step2),
+                view.findViewById(R.id.activity_steppers_container_step3)
         ));
         for (View b : buttons) {
             b.setOnClickListener(this);
@@ -66,45 +47,15 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         int id = view.getId();
         switch (id) {
-            case R.id.activity_steppers_txt_label_step1:
-                if (completeStep >= 0 && currentStep != 0) {
-                    currentStep = 0;
-                    closeStep();
-                    AnimationHelper.slideDownIn(layoutList.get(0))
-                            .interpolator(new LinearOutSlowInInterpolator())
-                            .duration(50)
-                            .start();
-                }
+            case R.id.activity_steppers_container_step1:
+                // do something here for step 1
                 break;
-            case R.id.activity_steppers_txt_label_step2:
-                if (completeStep >= 1 && currentStep != 1) {
-                    currentStep = 1;
-                    closeStep();
-                    AnimationHelper.slideDownIn(layoutList.get(1))
-                            .interpolator(new LinearOutSlowInInterpolator())
-                            .duration(50)
-                            .start();
-                }
+            case R.id.activity_steppers_container_step2:
+                // do something here for step 2
                 break;
-            case R.id.activity_steppers_txt_label_step3:
-                if (completeStep >= 2 && currentStep != 2) {
-                    currentStep = 2;
-                    closeStep();
-                    AnimationHelper.slideDownIn(layoutList.get(2))
-                            .interpolator(new LinearOutSlowInInterpolator())
-                            .duration(50)
-                            .start();
-                }
+            case R.id.activity_steppers_container_step3:
+                // do something here for step 3
                 break;
-        }
-    }
-
-    private void closeStep() {
-        for (View v : layoutList) {
-            AnimationHelper.slideUpOut(v)
-                    .interpolator(new LinearOutSlowInInterpolator())
-                    .duration(50)
-                    .start();
         }
     }
 
