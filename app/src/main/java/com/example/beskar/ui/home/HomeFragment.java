39e8488615804ee429d72868e65c0dd711c64202
Gerald Low <bcl15@ic.ac.uk>
2020-12-28 18:20:33 +0000

    WIP remove collapse behavior

--- app/src/main/java/com/example/beskar/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/beskar/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,9 +23,27 @@ import java.util.List;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    private class StepState {
+        public View container;
+        public TextView label;
+        public ImageView labelArrow;
+        public ImageView bottomSheetArrow;
+        public BottomSheetBehavior bottomSheetBehavior;
+        public boolean on;
+        public StepState(View container, TextView label, ImageView labelArrow,
+                         ImageView bottomSheetArrow, BottomSheetBehavior bottomSheetBehavior) {
+            this.container = container;
+            this.label = label;
+            this.labelArrow = labelArrow;
+            this.bottomSheetArrow = bottomSheetArrow;
+            this.bottomSheetBehavior = bottomSheetBehavior;
+            this.on = false;
+        }
+    }
+
     private HomeViewModel homeViewModel;
     private View root;
-    private BottomSheetBehavior bottomSheetBehavior;
+    private List<StepState> stepStates;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,17 +55,62 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     }
 
     private void initComponent(View view) {
-        // Register buttons
-        List<View> buttons = new ArrayList<>(Arrays.asList(
-                view.findViewById(R.id.activity_steppers_container_step1),
-                view.findViewById(R.id.activity_steppers_container_step2),
-                view.findViewById(R.id.activity_steppers_container_step3)
+        // Build list of stepStates
+        stepStates = new ArrayList<>(Arrays.asList(
+                new StepState(
+                        view.findViewById(R.id.activity_steppers_container_step1),
+                        (TextView) view.findViewById(R.id.activity_steppers_txt_label_step1),
+                        (ImageView) view.findViewById(R.id.activity_steppers_expand_button_step1),
+                        (ImageView) view.findViewById(R.id.activity_bottom_sheet_step1_expand_button),
+                        BottomSheetBehavior.from(
+                                view.findViewById(R.id.bottom_sheet_step1))
+                ),
+                new StepState(
+                        view.findViewById(R.id.activity_steppers_container_step2),
+                        (TextView) view.findViewById(R.id.activity_steppers_txt_label_step2),
+                        (ImageView) view.findViewById(R.id.activity_steppers_expand_button_step2),
+                        (ImageView) view.findViewById(R.id.activity_bottom_sheet_step2_expand_button),
+                        BottomSheetBehavior.from(
+                                view.findViewById(R.id.bottom_sheet_step2))
+                ),
+                new StepState(
+                        view.findViewById(R.id.activity_steppers_container_step3),
+                        (TextView) view.findViewById(R.id.activity_steppers_txt_label_step3),
+                        (ImageView) view.findViewById(R.id.activity_steppers_expand_button_step3),
+                        (ImageView) view.findViewById(R.id.activity_bottom_sheet_step3_expand_button),
+                        BottomSheetBehavior.from(
+                                view.findViewById(R.id.bottom_sheet_step3))
+                )
         ));
-        for (View b : buttons) {
-            b.setOnClickListener(this);
+
+        // Set initial state
+        for (StepState stepState : stepStates) {
+            stepState.container.setOnClickListener(this);
+            stepState.bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            stepState.bottomSheetBehavior.addBottomSheetCallback(
+                    new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                    switch (newState) {
+                        case BottomSheetBehavior.STATE_COLLAPSED:
+                            stepState.bottomSheetArrow
+                                    .setBackgroundResource(R.drawable.baseline_expand_less_24);
+                            break;
+                        case BottomSheetBehavior.STATE_EXPANDED:
+                            stepState.bottomSheetArrow
+                                    .setBackgroundResource(R.drawable.baseline_expand_more_24);
+                            break;
+                        case BottomSheetBehavior.STATE_HIDDEN:
+                            toggle(stepState, false);
+                    }
+                }
+
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
+            });
         }
 
-        // Set slider state
+        // Set slider state in step 1
         List<String> sliderTexts = new ArrayList<>(Arrays.asList(
                 "No filter applied. System default settings used.",
                 "Malicious domains (e.g. phishing, malware) blocked.",
@@ -54,17 +118,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 "Proxies, VPNs & Mixed adult content blocked. Youtube to safe mode. Adult domains" +
                         " blocked. Search engines to safe mode. Malicious domains blocked."
         ));
-        Slider slider = view.findViewById(R.id.activity_bottom_sheet_slider);
-        TextView tv = view.findViewById(R.id.activity_bottom_sheet_slider_txt_label);
+        Slider slider = view.findViewById(R.id.activity_bottom_sheet_step1_slider);
+        TextView tv = view.findViewById(R.id.activity_bottom_sheet_step1_slider_txt_label);
         slider.addOnChangeListener((s, value, fromUser) -> {
             tv.setText(sliderTexts.get(Math.round(value)));
         });
-
-        // Set bottom sheet behavior
-        bottomSheetBehavior = BottomSheetBehavior.from(
-                view.findViewById(R.id.bottom_sheet)
-        );
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
 
         // Hide keyboard
         hideKeyboard();
@@ -75,72 +133,44 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         int id = view.getId();
         switch (id) {
             case R.id.activity_steppers_container_step1:
-                // Select this step
-                toggle(R.id.activity_steppers_txt_label_step1,
-                        R.id.activity_steppers_expand_button_step1, true);
-
-                // Unselect other steps
-                toggle(R.id.activity_steppers_txt_label_step2,
-                        R.id.activity_steppers_expand_button_step2, false);
-                toggle(R.id.activity_steppers_txt_label_step3,
-                        R.id.activity_steppers_expand_button_step3, false);
+                onClickHelper(0);
                 break;
             case R.id.activity_steppers_container_step2:
-                // Select this step
-                toggle(R.id.activity_steppers_txt_label_step2,
-                        R.id.activity_steppers_expand_button_step2, true);
-
-                // Unselect other steps
-                toggle(R.id.activity_steppers_txt_label_step1,
-                        R.id.activity_steppers_expand_button_step1, false);
-                toggle(R.id.activity_steppers_txt_label_step3,
-                        R.id.activity_steppers_expand_button_step3, false);
+                onClickHelper(1);
                 break;
             case R.id.activity_steppers_container_step3:
-                // Select this step
-                toggle(R.id.activity_steppers_txt_label_step3,
-                        R.id.activity_steppers_expand_button_step3, true);
-
-                // Unselect other steps
-                toggle(R.id.activity_steppers_txt_label_step1,
-                        R.id.activity_steppers_expand_button_step1, false);
-                toggle(R.id.activity_steppers_txt_label_step2,
-                        R.id.activity_steppers_expand_button_step2, false);
+                onClickHelper(2);
                 break;
         }
     }
 
-    private void toggle(int label, int button, boolean on) {
+    private void onClickHelper(int chosen) {
+        for (int i = 0; i < stepStates.size(); i++) {
+            if (i == chosen) {
+                // Toggle the chosen step
+                StepState chosenStepState = stepStates.get(chosen);
+                toggle(chosenStepState, !chosenStepState.on);
+            } else {
+                // Turn off all other steps
+                toggle(stepStates.get(i), false);
+            }
+        }
+    }
+
+    private void toggle(StepState stepState, boolean turn_on) {
         TextViewCompat.setTextAppearance(
-                root.findViewById(label),
-                on ? R.style.StepTextSelectedAppearance : R.style.StepTextUnselectedAppearance
+                stepState.label,
+                turn_on ? R.style.StepTextSelectedAppearance : R.style.StepTextUnselectedAppearance
         );
-        root.findViewById(button)
+        stepState.labelArrow
                 .setBackgroundResource(
-                        on ? R.drawable.baseline_expand_less_24 : R.drawable.baseline_expand_more_24
+                        turn_on ? R.drawable.baseline_expand_less_24 :
+                                R.drawable.baseline_expand_more_24
                 );
-        bottomSheetBehavior.setState(on ? BottomSheetBehavior.STATE_EXPANDED :
+        stepState.bottomSheetBehavior.setState(turn_on ? BottomSheetBehavior.STATE_EXPANDED :
                 BottomSheetBehavior.STATE_HIDDEN);
 
-        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                int bottomSheetArrow = R.id.activity_bottom_sheet_expand_button;
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        root.findViewById(bottomSheetArrow)
-                                .setBackgroundResource(R.drawable.baseline_expand_less_24);
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        root.findViewById(bottomSheetArrow)
-                                .setBackgroundResource(R.drawable.baseline_expand_more_24);
-                        break;
-                }
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
-        });
+        stepState.on = turn_on;
     }
 
     public void hideKeyboard() {
