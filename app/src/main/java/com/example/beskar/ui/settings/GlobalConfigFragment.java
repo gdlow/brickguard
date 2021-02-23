2919fbaee9140c9f02f813ea624db6a17d102084
Gerald Low <bcl15@ic.ac.uk>
2021-02-23 16:38:39 +0000

    WIP: Add email alertdialog UI

--- app/src/main/java/com/example/beskar/ui/settings/GlobalConfigFragment.java
+++ app/src/main/java/com/example/beskar/ui/settings/GlobalConfigFragment.java
@@ -1,14 +1,42 @@
 package com.example.beskar.ui.settings;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.beskar.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class GlobalConfigFragment extends PreferenceFragmentCompat {
+
+    private MaterialAlertDialogBuilder materialAlertDialogBuilder;
+    private View emailDialog;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_settings);
+        materialAlertDialogBuilder = new MaterialAlertDialogBuilder(getContext());
+        findPreference("settings_email").setOnPreferenceClickListener(preference -> {
+            emailDialog = LayoutInflater.from(getContext()).inflate(R.layout.email_dialog, null,
+                    false);
+            launchEmailDialog();
+            return true;
+        });
+    }
+
+    private void launchEmailDialog() {
+        // Build the alert dialog
+        materialAlertDialogBuilder.setView(emailDialog)
+                .setPositiveButton("Set", (dialog, which) -> {
+                    // Do whatever you want here
+                })
+                .setNegativeButton("Cancel", ((dialog, which) -> {
+                    Toast.makeText(getContext(), "Operation cancelled!", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }))
+                .show();
     }
 }
